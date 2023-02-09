7eece57e07bc9abb07d33e44bef211832caca733
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-10 00:02:57 +0100

    Update 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,4 +2,8 @@
 ---     id, integer, never null, auto increment and primary key
 ---     email, string (255 characters), never null and unique
 ---    name, string (255 characters)
-CREATE TABLE IF NOT EXISTS users (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, email VARCHAR(255) NOT NULL UNIQUE, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
