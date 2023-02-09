b0ca2733972129588047966fd102cf2695d4cbe5
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-10 00:59:33 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, (ifnull(split, '2020') - formed) AS lifespan
-FROM metal_bands WHERE style == 'GLAM ROCK'
+FROM metal_bands WHERE style == 'Glam rock'
 ORDER BY lifespan DESC;
