5c18c55f6eae1de78e510312cbafb40cd9742831
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-10 12:27:14 +0100

    Update 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,2 @@
 --  SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
-DELIMITER $$ ;
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
-BEGIN
-    CREATE INDEX idx_name_first ON names (name(1));
-END;$$
-DELIMITER ;
+CREATE INDEX idx_name_first ON names (name(1));
