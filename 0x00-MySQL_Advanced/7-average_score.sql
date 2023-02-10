c064d2f112ee99d424122533cb5d0f90339880d5
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-10 12:07:45 +0100

    Create 7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+--  SQL script that creates a stored procedure ComputeAverageScoreForUser
+--  that computes and store the average score for a student.
+DELIMITER $$ ;
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET score = (SELECT AVG(score) FROM corrections WHERE id = user_id)
+    WHERE id = user_id;
+END;$$
+DELIMITER ;
