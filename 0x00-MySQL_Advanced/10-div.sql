6359f8428ed6463847bd53ed99b18524669306ba
God'sfavour Idowu-Agida <favouridowuagida5@gmail.com>
2023-02-10 12:56:57 +0100

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,7 +2,7 @@
 -- divides (and returns) the first by the second number
 -- or returns 0 if the second number is equal to 0
 DELIMITER $$ ;
-CREATE FUNCTION SafeDiv (IN a INT, IN b INT) RETURNS FLOAT
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT
 BEGIN
     IF b = 0 THEN
         RETURN 0;
